4a580cb43459a8e0af2190297c01d049e958cf56
Rhys Johns <rhys.johns@energyaction.com.au>
2021-11-09 17:05:34 +1100

    - Added attribute for retention and compression policies in TimescaleDb

---
+++ EfYou/Model/Attributes/TimescaleCompressionPolicyAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EfYou.Model.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class TimescaleCompressionPolicyAttribute : Attribute
+    {
+        // Intervals being assigned in should match the form of:
+        //      1 day
+        //      2 months
+        //      28 days
+        //      14 years
+        public string Interval { get; set; }
+    }
+}
