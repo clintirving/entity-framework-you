704c90a72905517c2a0f0e797b1e8ef66649fbf5
Rhys Johns <rhys.johns@energyaction.com.au>
2021-11-09 16:09:15 +1100

    - Implemented method to pull all the available entities from a db context

---
+++ EfYou/Extensions/ModelBuilderExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+
+namespace EfYou.Extensions
+{
+    public static class ModelBuilderExtensions
+    {
+        public static List<Type> GetAllDatabaseEntities(this DbModelBuilder modelBuilder)
+        {
+            var modelConfiguration = modelBuilder.GetType().GetProperty("ModelConfiguration",
+                System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.NonPublic |
+                System.Reflection.BindingFlags.Instance);
+
+            var configuration = modelConfiguration?.GetValue(modelBuilder);
+
+            var configuredTypes = configuration?.GetType().GetProperty("ConfiguredTypes",
+                System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.NonPublic |
+                System.Reflection.BindingFlags.Instance);
+
+            var entities = (IEnumerable<Type>) configuredTypes?.GetValue(configuredTypes, null);
+
+            return entities?.ToList();
+        }
+    }
+}
