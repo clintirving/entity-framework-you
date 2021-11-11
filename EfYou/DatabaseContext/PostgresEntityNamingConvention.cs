2cacbb7347df73030aa907b0d471bc396561b2a8
Rhys Johns <rhysjohnsdev@gmail.com>
2021-11-11 15:57:17 +1100

    - Added new naming conventions to ensure that any postgres names are automatically turned into lower case

---
+++ EfYou/DatabaseContext/PostgresEntityNamingConvention.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity.Core.Metadata.Edm;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using EfYou.EntityServices;
+
+namespace EfYou.DatabaseContext
+{
+    public class PostgresEntityNamingConvention : IStoreModelConvention<EntitySet>
+    {
+        public void Apply(EntitySet item, DbModel model)
+        {
+            item.Table = item.Table.ToLower();
+        }
+    }
+}
---
+++ EfYou/DatabaseContext/PostgresPropertyNamingConvention.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity.Core.Metadata.Edm;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace EfYou.DatabaseContext
+{
+    public class PostgresPropertyNamingConvention : IStoreModelConvention<EdmProperty>
+    {
+        public void Apply(EdmProperty item, DbModel model)
+        {
+            item.Name = item.Name.ToLower();
+        }
+    }
+}
